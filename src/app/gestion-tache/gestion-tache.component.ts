5847dc5cb58937a284f4d95d62687086e29d5e08
killianchappe <killian.chappe@gmail.com>
2019-12-12 14:36:55 +0100

    FrontProjetZoo - v5 - Sauvegarde avant calendar - 12/12/2019

--- src/app/gestion-tache/gestion-tache.component.ts
+++ src/app/gestion-tache/gestion-tache.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { UserService } from '../services/user/user.service';
+import { Tache } from '../models/tache';
+import { TacheService } from '../services/tache/tache.service';
+import Swal from 'sweetalert2';
+import { Etat } from '../models/etat';
+import { EtatService } from '../services/etat/etat.service';
 
 @Component({
   selector: 'app-gestion-tache',
@@ -7,9 +16,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionTacheComponent implements OnInit {
 
-  constructor() { }
+  listUsers: User[] = [];
+  listEtats: Etat[] = [];
+  newTache: Tache = new Tache();
+  myFormTache: FormGroup;
+
+  constructor(private userService: UserService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private tacheService: TacheService,
+    private etatService: EtatService) { }
 
   ngOnInit() {
+    this.userService.getAll().subscribe(data => {
+      this.listUsers = data;
+    });
+    this.etatService.getAll().subscribe(data => {
+      this.listEtats = data;
+    });
+    this.myFormTache = this.formBuilder.group({
+      libelleTache: ['', Validators.required],
+      commentaireTache: ['', Validators.required],
+      dateTache: ['', Validators.required],
+      dureeTache: ['', Validators.required],
+      userTache: ['', Validators.required],
+    });
+  }
+
+  onResetTache() {
+    this.myFormTache.reset();
+  }
+
+  ajoutTache() {
+    this.newTache.etatTache = this.listEtats[0];
+    this.tacheService.addTache(this.newTache).subscribe(res => {
+      if (res) {
+        Swal.fire({
+          title: 'Tâche ajoutée!',
+          icon: 'success',
+          focusConfirm: true,
+          confirmButtonText: 'OK',
+        }).then((result) => {
+          if (result.value) {
+            this.ngOnInit();
+          }
+        })
+      }
+    });
   }
 
 }
